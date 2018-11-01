f7873c014c0d9896070c331fe22ea850617f965d
Ac7884 <ace7884@hotmail.com>
2018-11-01 02:46:02 +0200

    organized js

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -2,9 +2,6 @@ var Trackster ={};
 
 $(document).ready(function(){
 
-  
-
-
   /*
     Given an array of track data, create the HTML for a Bootstrap row for each.
     Append each "row" to the container in the body to display all tracks. 
@@ -16,21 +13,20 @@ $(document).ready(function(){
 // loop iriterating the function n requested times//
 
 for ( i=0 ; i<tracks.length ; i++ ) {
-let track=tracks[i];
-let mediumAlbumArt=track.image[1]["#text"];
-let trackinfo='<div class="container-fluid">'
-+'<div id="mock-row" class="row form-inline" >'
-+'<div  class="col-2"><a href="https://www.youtube.com/watch?v=eI_O5_tJ1hA&feature=youtu.be"><i class="far fa-play-circle fa-lg"></i></a></div>'
-+'<div  class="col-4">'+i+'&nbsp;'+ track.name+'</div>' 
-+'<div  class="col-2">'+track.artist+'</div>' 
-+'<div  class="col-2"><img src='+mediumAlbumArt+'></div>'
-+'<div  class="col-2">'+track.listeners+'</div>'
-+'</div>';
+  let track=tracks[i];
+  let mediumAlbumArt=track.image[1]["#text"];
+  let trackinfo='<div class="container-fluid">'
+  +'<div id="mock-row" class="row form-inline" >'
+  +'<div  class="col-2"><a href="https://www.youtube.com/watch?v=eI_O5_tJ1hA&feature=youtu.be"><i class="far fa-play-circle fa-lg"></i></a></div>'
+  +'<div  class="col-4">'+i+'&nbsp;'+ track.name+'</div>' 
+  +'<div  class="col-2">'+track.artist+'</div>' 
+  +'<div  class="col-2"><img src='+mediumAlbumArt+'></div>'
+  +'<div  class="col-2">'+track.listeners+'</div>'
+  +'</div>';
 
-$('#results').append(trackinfo); 
+  $('#results').append(trackinfo); 
     
-  }
-
+}
 
 };
 
@@ -39,17 +35,10 @@ $('#results').append(trackinfo);
       Render the tracks given in the API query response.
       */
 
-  
- 
-
     $('#search-btn').click(function() {
-  
-     
-    
+ 
       const title= $('input').val();
-      
-     
-    
+  
       Trackster.searchTracksByTitle = function( title) {
         const api_Key= 'd9b112d3110fd24c4e07b05eae2b3dc1';
          
@@ -64,13 +53,6 @@ $('#results').append(trackinfo);
     
       Trackster.searchTracksByTitle(title);
     });
-     
-
-  
-
-  
-  
-  
   
 });
 
