2429517c6231be0cc3693841c202095dcd0da978
Amnon Carmi <ace7884@hotmail.com>
2023-01-03 20:12:04 +0200

    Refractoring

--- resources/js/tests.js
+++ resources/js/tests.js
@@ -1,79 +1,107 @@
 var TracksterTests = {};
 
-TracksterTests.runAllTracksterTests = function() {
+TracksterTests.runAllTracksterTests = function () {
   var errors = [];
   TracksterTests.testSearchTracksByTitle(errors);
   TracksterTests.testRenderTracks(errors);
   TracksterTests.logErrors(errors);
 };
 
-TracksterTests.testRenderTracks = function(errors) {
-  var hasRenderTracks = Trackster.renderTracks && typeof Trackster.renderTracks === 'function';
+TracksterTests.testRenderTracks = function (errors) {
+  var hasRenderTracks =
+    Trackster.renderTracks && typeof Trackster.renderTracks === "function";
   if (!hasRenderTracks) {
-    errors.push("renderTracks: Trackster object should have a function called renderTracks.");
+    errors.push(
+      "renderTracks: Trackster object should have a function called renderTracks."
+    );
     // If renderTracks method is missing, remaining tests will not work.
     return;
   }
 
-  var userHtml = $('body').html();
+  var userHtml = $("body").html();
 
-  var tinyDancerTracks = [{
-    url: 'https://www.last.fm/music/Elton+John/_/Tiny+Dancer',
-    name: 'Tiny Dancer',
-    artist: 'Elton John',
-    image: [
-      {},
-      {
-        '#text': 'https://lastfm-img2.akamaized.net/i/u/64s/e4b1e25d34694b5d89541185ef45cfa3.png'
-      },
-      {},
-      {}
-    ],
-    listeners: '581520'
-  }];
+  var tinyDancerTracks = [
+    {
+      url: "https://www.last.fm/music/Elton+John/_/Tiny+Dancer",
+      name: "Tiny Dancer",
+      artist: "Elton John",
+      image: [
+        {},
+        {
+          "#text":
+            "https://lastfm-img2.akamaized.net/i/u/64s/e4b1e25d34694b5d89541185ef45cfa3.png",
+        },
+        {},
+        {},
+      ],
+      listeners: "581520",
+    },
+  ];
   Trackster.renderTracks(tinyDancerTracks);
-  if (!$("a[href='https://www.last.fm/music/Elton+John/_/Tiny+Dancer']").length) {
-    errors.push("renderTracks: renderTracks should add an <a> tag link to each track's preview url.");
+  if (
+    !$("a[href='https://www.last.fm/music/Elton+John/_/Tiny+Dancer']").length
+  ) {
+    errors.push(
+      "renderTracks: renderTracks should add an <a> tag link to each track's preview url."
+    );
   }
   if (!$("body:contains('Tiny Dancer')").length) {
-    errors.push("renderTracks: renderTracks should add each track's name to the track list.");
+    errors.push(
+      "renderTracks: renderTracks should add each track's name to the track list."
+    );
   }
   if (!$("body:contains('Elton John')").length) {
-    errors.push("renderTracks: renderTracks should add each track's artist's name to the track list.");
+    errors.push(
+      "renderTracks: renderTracks should add each track's artist's name to the track list."
+    );
   }
-  if (!$("img[src='https://lastfm-img2.akamaized.net/i/u/64s/e4b1e25d34694b5d89541185ef45cfa3.png']").length) {
-    errors.push("renderTracks: renderTracks should add each track's album art to the track list.");
+  if (
+    !$(
+      "img[src='https://lastfm-img2.akamaized.net/i/u/64s/e4b1e25d34694b5d89541185ef45cfa3.png']"
+    ).length
+  ) {
+    errors.push(
+      "renderTracks: renderTracks should add each track's album art to the track list."
+    );
   }
   if (!$("body:contains('581520')").length) {
-    errors.push("renderTracks: renderTracks should add each track's total number of listeners to the track list.");
+    errors.push(
+      "renderTracks: renderTracks should add each track's total number of listeners to the track list."
+    );
   }
 
   Trackster.renderTracks([]);
   if ($("body:contains('Tiny Dancer')").length) {
-    errors.push("renderTracks: renderTracks should empty the HTML of the track list before rendering a new list of tracks.");
+    errors.push(
+      "renderTracks: renderTracks should empty the HTML of the track list before rendering a new list of tracks."
+    );
   }
 
-  $('body').html(userHtml);
+  $("body").html(userHtml);
 };
 
-TracksterTests.testSearchTracksByTitle = function(errors) {
-  var hasSearchTracksByTitle = Trackster.searchTracksByTitle && typeof Trackster.searchTracksByTitle === 'function';
+TracksterTests.testSearchTracksByTitle = function (errors) {
+  var hasSearchTracksByTitle =
+    Trackster.searchTracksByTitle &&
+    typeof Trackster.searchTracksByTitle === "function";
   if (!hasSearchTracksByTitle) {
-    errors.push("searchTracksByTitle: Trackster object should have a function called searchTracksByTitle.");
+    errors.push(
+      "searchTracksByTitle: Trackster object should have a function called searchTracksByTitle."
+    );
     // If searchTracksByTitle method is missing, remaining tests will not work.
     return;
   }
 
-  var mockAjax = TracksterTests.mock($, 'ajax');
+  var mockAjax = TracksterTests.mock($, "ajax");
 
-  Trackster.searchTracksByTitle('tiny');
+  Trackster.searchTracksByTitle("tiny");
 
   if (!mockAjax.getCalls().length) {
     errors.push("searchTracksByTitle: searchTracksByTitle should call $.ajax.");
   } else {
     var url, successFn;
     var call = mockAjax.getCalls()[0];
-    if (typeof call[0] === 'string') {
+    if (typeof call[0] === "string") {
       url = call[0];
       successFn = call[1] && call[1].success;
     } else {
@@ -82,24 +110,38 @@ TracksterTests.testSearchTracksByTitle = function(errors) {
     }
 
     if (!url) {
-      errors.push('searchTracksByTitle: $.ajax must specify a "url" parameter.');
+      errors.push(
+        'searchTracksByTitle: $.ajax must specify a "url" parameter.'
+      );
     } else {
-      var match = url.match(/https:\/\/ws\.audioscrobbler\.com\/2\.0\/\?method=track\.search&track=.*&api_key=([a-z]|\d){32}&format=json/);
+      var match = url.match(
+        /https:\/\/ws\.audioscrobbler\.com\/2\.0\/\?method=track\.search&track=.*&api_key=([a-z]|\d){32}&format=json/
+      );
       if (!match) {
-        errors.push("searchTracksByTitle: $.ajax url should hit the ws.audioscrobbler.com/2.0/method=track.search endpoint.");
+        errors.push(
+          "searchTracksByTitle: $.ajax url should hit the ws.audioscrobbler.com/2.0/method=track.search endpoint."
+        );
       } else {
-        params = match[0].split('&');
+        params = match[0].split("&");
         if (params.length < 2) {
-          errors.push('searchTracksByTitle: $.ajax url should have parameters for "track", "api_key", and "format" separated by an "&".');
+          errors.push(
+            'searchTracksByTitle: $.ajax url should have parameters for "track", "api_key", and "format" separated by an "&".'
+          );
         } else {
-          if (!params.includes('track=tiny')) {
-            errors.push('searchTracksByTitle: $.ajax url should have a parameter called "track" with a value of the supplied title parameter.');
+          if (!params.includes("track=tiny")) {
+            errors.push(
+              'searchTracksByTitle: $.ajax url should have a parameter called "track" with a value of the supplied title parameter.'
+            );
           } else {
             if (!params[2].match(/api_key=([a-z]|\d){32}/)) {
-              errors.push('searchTracksByTitle: $.ajax url should have a parameter called "api_key" set to a valid API key.');
+              errors.push(
+                'searchTracksByTitle: $.ajax url should have a parameter called "api_key" set to a valid API key.'
+              );
             } else {
-              if (!params.includes('format=json')) {
-                errors.push('searchTracksByTitle: $.ajax url should have a parameter called "format" with a value of "json".');
+              if (!params.includes("format=json")) {
+                errors.push(
+                  'searchTracksByTitle: $.ajax url should have a parameter called "format" with a value of "json".'
+                );
               }
             }
           }
@@ -108,26 +150,32 @@ TracksterTests.testSearchTracksByTitle = function(errors) {
     }
 
     if (!successFn) {
-      errors.push('searchTracksByTitle: $.ajax must specify a "success" parameter.');
+      errors.push(
+        'searchTracksByTitle: $.ajax must specify a "success" parameter.'
+      );
     } else {
-      var mockRender = TracksterTests.mock(Trackster, 'renderTracks');
-      var mockItems = 'Tiny Dancer';
+      var mockRender = TracksterTests.mock(Trackster, "renderTracks");
+      var mockItems = "Tiny Dancer";
       var response = {
         results: {
           trackmatches: {
             track: [
               {
-                name: mockItems
-              }
-            ]
-          }
-        }
+                name: mockItems,
+              },
+            ],
+          },
+        },
       };
       successFn(response);
       if (!mockRender.getCalls().length) {
-        errors.push('searchTracksByTitle: "success" callback should call Trackster.renderTracks().');
+        errors.push(
+          'searchTracksByTitle: "success" callback should call Trackster.renderTracks().'
+        );
       } else if (mockRender.getCalls()[0][0][0].name !== mockItems) {
-        errors.push("searchTracksByTitle: Trackster.renderTracks() should be called with data located in the returned response's `data`.");
+        errors.push(
+          "searchTracksByTitle: Trackster.renderTracks() should be called with data located in the returned response's `data`."
+        );
       }
       mockRender.restore();
     }
@@ -135,34 +183,34 @@ TracksterTests.testSearchTracksByTitle = function(errors) {
   mockAjax.restore();
 };
 
-TracksterTests.mock = function(object, functionName) {
+TracksterTests.mock = function (object, functionName) {
   var oldFn = object[functionName];
   var calls = [];
-  object[functionName] = function() {
+  object[functionName] = function () {
     calls.push(Array.prototype.slice.apply(arguments));
   };
   return {
-    restore: function() {
+    restore: function () {
       object[functionName] = oldFn;
     },
-    getCalls: function() {
+    getCalls: function () {
       return calls;
-    }
+    },
   };
 };
 
-TracksterTests.logErrors = function(errors) {
+TracksterTests.logErrors = function (errors) {
   if (errors.length === 0) {
-    console.log('%cAll TracksterTests passed!', 'color: #30AD35');
+    console.log("%cAll TracksterTests passed!", "color: #30AD35");
   } else {
     errors = new Set(errors);
-    var errorMessage = ' errors found:';
+    var errorMessage = " errors found:";
     if (errors.size === 1) {
-      errorMessage = ' error found:';
+      errorMessage = " error found:";
     }
-    console.log('%c' + errors.size + ' ' + errorMessage, 'color: #BA1222');
-    errors.forEach(function(error) {
-      console.log('%c  ' + error, 'color: #BA1222');
+    console.log("%c" + errors.size + " " + errorMessage, "color: #BA1222");
+    errors.forEach(function (error) {
+      console.log("%c  " + error, "color: #BA1222");
     });
   }
 };
--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,96 +1,97 @@
 var Trackster = {};
-const api_Key = 'd9b112d3110fd24c4e07b05eae2b3dc1';
-$(document).ready(function() {
-    $('#search-btn').click(function() {
-        Trackster.searchTracksByTitle($('input').val());
+const api_Key = "d9b112d3110fd24c4e07b05eae2b3dc1";
+$(document).ready(function () {
+  $("#search-btn").click(function () {
+    Trackster.searchTracksByTitle($("input").val());
+  });
 
-    });
+  // animates trackster while loading track results
 
-    // animates trackster while producting track results
+  $(document).ajaxStart(function () {
+    $("h1").addClass("load");
+  });
 
+  // turns off trackster animation when found results
 
+  $(document).ajaxSuccess(function () {
+    $("h1").removeClass("load");
+  });
 
-$(document).ajaxStart(function (){
-    $('h1').addClass('load'); 
+  // if press return activate search function
+
+  $("input").keydown(function (keyPressed) {
+    if (keyPressed.which === 13) {
+      Trackster.searchTracksByTitle($("input").val());
     }
-); 
-
-// if press return activate search function
-
-    $('input').keydown(function(keyPressed) {
-        if ( keyPressed.which === 13 ) {
-            Trackster.searchTracksByTitle($('input').val());
-        }
-    });    
+  });
 });
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks. 
 */
-Trackster.renderTracks = function(tracks) {
-    
+Trackster.renderTracks = function (tracks) {
+  $("#results").empty();
 
-    $('#results').empty();
+  // loop iriterating the function n requested times//
 
-    // loop iriterating the function n requested times//
+  for (i = 0; i < tracks.length; i++) {
+    let track = tracks[i];
+    let mediumAlbumArt = track.image[1]["#text"];
+    console.log("mediumAlbumArt", mediumAlbumArt);
+    track.listeners = numeral(track.listeners).format("0,0");
+    let htmlTrackInfo =
+      '<div class="container-fluid">' +
+      '<div id="mock-row" class="row form-inline" >' +
+      '<div  class="col-2"><a href=" ' +
+      track.url +
+      '" target="_blank"><i class="far fa-play-circle fa-lg"></i></a></div>' +
+      '<div  class="col-4">' +
+      i +
+      "&nbsp;" +
+      track.name +
+      "</div>" +
+      '<div  class="col-2">' +
+      track.artist +
+      "</div>" +
+      '<div  class="col-2"><img src=' +
+      mediumAlbumArt +
+      "></div>" +
+      '<div  class="col-2">' +
+      track.listeners +
+      "</div>" +
+      "</div>";
 
-    for (i = 0; i < tracks.length; i++) {
-        let track = tracks[i];
-        let mediumAlbumArt = track.image[1]["#text"];
-        track.listeners = numeral(track.listeners).format('0,0');
-        let htmlTrackInfo = '<div class="container-fluid">' +
-            '<div id="mock-row" class="row form-inline" >' +
-            '<div  class="col-2"><a href=" '+ track.url +'" target="_blank"><i class="far fa-play-circle fa-lg"></i></a></div>' +
-            '<div  class="col-4">' + i + '&nbsp;' + track.name + '</div>' +
-            '<div  class="col-2">' + track.artist + '</div>' +
-            '<div  class="col-2"><img src=' + mediumAlbumArt + '></div>' +
-            '<div  class="col-2">' + track.listeners + '</div>' +
-            '</div>';
-
-        $('#results').append(htmlTrackInfo);
-        $('#results').data( 'row'+[i] , htmlTrackInfo );
-        console.log($('#results').data( 'row'+[i] ));
-
-        
-    };    
-
+    $("#results").append(htmlTrackInfo);
+    $("#results").data("row" + [i], htmlTrackInfo);
+    console.log(track);
+  }
 };
-
 
 /*
 Given a search term as a string, query the LastFM API.
 Render the tracks given in the API query response.
 */
 
+Trackster.searchTracksByTitle = function (title) {
+  $.ajax({
+    url:
+      "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" +
+      title +
+      "&api_key=" +
+      api_Key +
+      "&format=json",
+    success: function (response) {
+      $("h1").addClass("load");
+      Trackster.renderTracks(response.results.trackmatches.track);
+    },
+  });
+};
 
+// sorting function for attributes
 
-    Trackster.searchTracksByTitle = function(title) {
-        $.ajax({
-            url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + api_Key + '&format=json',
-            success: function(response) {
-                $('h1').addClass('load'); 
-                console.log(response)
-                Trackster.renderTracks(response.results.trackmatches.track);
-                
-            }
-        });
-    };
-
-    
-
-   // sorting function for attributes
-
-   Trackster.sortTracksByAttribute = function() {
-   $('.attribute').click(function() {
-
-    console.log($('#results').data( 'row'+[i] ));
-
-
-
-});
-
-
-
-
+Trackster.sortTracksByAttribute = function () {
+  $(".attribute").click(function () {
+    console.log($("#results").data("row" + [i]));
+  });
 };
